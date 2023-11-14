39fa72c94815dfbf5c200fc18614118d47ec9f92
hiwoogi <rmcprpwjd001@gmail.com>
2023-11-14 15:06:38 +0900

    실검 카테고리 설정

--- src/components/Trend.js
+++ src/components/Trend.js
@@ -8,11 +8,12 @@ export default function Trend() {
         keywords: [],
         
       });
+    const [field, setField] = useState('50000000')
 
     useEffect(() => {
         const fetchData = async () => {
           try {
-            const response = await axios.get('http://localhost:8080/test2/scrape/50000000');
+            const response = await axios.get(`http://localhost:8080/test2/scrape/${field}`);
             console.log('Response:', response.data);
             const arr = response.data
             const latestData = arr[arr.length-1]
@@ -27,14 +28,39 @@ export default function Trend() {
         };
     
         fetchData(); // Call the async function
-      }, []);  
+      }, [field]);  
     
 
   return (
-    <div id='treemap'>
-        <TreeMap
+    <div>
+        <div className="flex w-[356px] max-w-full items-start justify-between gap-5 ml-5 mt-20 self-start max-md:ml-2.5 max-md:mt-10">
+            <div className="text-black text-xl leading-8 uppercase self-center my-auto">
+              분야
+            </div>
+
+            <select
+              name="category"
+              onChange={(e) =>
+                setField(e.target.value)
+              }
+              className="text-black text-base font-light leading-6 uppercase self-stretch border w-[269px] max-w-full grow shrink basis-auto items-start justify-between gap-5 pl-32 py-7 rounded-3xl border-solid border-black max-md:pl-5"
+            >
+              <option value="50000000">패션</option>
+              <option value="50000007">스포츠</option>
+              <option value="50000003">가전제품</option>
+              
+              <option value="50000006">음식</option>
+            </select>
+          </div>
+
+          <div id='treemap'>
+            <TreeMap
             trend = {trend}
         />
     </div>
+    </div>
+    
+
+   
   )
 }
