0dd6ac61f98b056c9cc6a5b11cb0891be1cadbfc
choijaewon000 <cjw9094@skuniv.ac.kr>
2023-11-17 14:19:50 +0900

    도서 아이콘 추가

--- src/components/main/TrendingInstruction.js
+++ src/components/main/TrendingInstruction.js
@@ -24,64 +24,59 @@ export default function TrendingInstruction(props) {
           className="aspect-[1.67] object-contain object-center w-[677px] overflow-hidden self-center max-w-full mt-12 max-md:mt-10"
         />
         <div className="self-center flex w-full items-start justify-between gap-5 mt-20 max-md:max-w-full max-md:flex-wrap max-md:justify-center max-md:mt-10">
-          <div className="shadow-sm bg-white flex flex-col flex-1 px-9 py-3 rounded-3xl max-md:px-5">
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/0ebb2b7c-b651-4c2b-8b2e-6f7834777987?apiKey=d9a6bade01504f228813cd0dfee9b81b&"
-              className="aspect-square object-contain object-center w-full overflow-hidden self-stretch"
-            />
-            <Link to={"/trend"} state={{fieldValue : 50000000}} 
-              className="text-blue-400 text-base font-semibold leading-5 tracking-normal self-center whitespace-nowrap font-['NEXON']"
-            >
-              패션
+          <div className="text-center shadow-sm bg-white flex flex-col flex-1 px-9 py-3 rounded-3xl max-md:px-5">
+            <Link to={"/trend"} state={{ fieldValue: 50000000 }}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/0ebb2b7c-b651-4c2b-8b2e-6f7834777987?apiKey=d9a6bade01504f228813cd0dfee9b81b&"
+                className="aspect-square object-contain object-center w-full overflow-hidden self-stretch"
+              />
+              <p className="text-blue-400 text-base font-semibold leading-5 tracking-normal self-center whitespace-nowrap font-['NEXON']">
+                패션 </p>
             </Link>
           </div>
-          <div className="shadow-sm bg-white flex flex-col flex-1 pt-2.5 pb-4 px-9 rounded-3xl max-md:px-5">
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/7108a0a4-bf35-43fb-80ca-453475259550?apiKey=d9a6bade01504f228813cd0dfee9b81b&"
-              className="aspect-square object-contain object-center w-full overflow-hidden self-stretch"
-            />
-            <Link to={"/trend"} state={{fieldValue : 50000007}} 
-              className="text-green-400 text-base font-semibold leading-5 tracking-normal self-center whitespace-nowrap font-['NEXON']"
-            >
-              스포츠
+          <div className="text-center shadow-sm bg-white flex flex-col flex-1 pt-2.5 pb-4 px-9 rounded-3xl max-md:px-5">
+            <Link to={"/trend"} state={{ fieldValue: 50000007 }}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/7108a0a4-bf35-43fb-80ca-453475259550?apiKey=d9a6bade01504f228813cd0dfee9b81b&"
+                className="aspect-square object-contain object-center w-full overflow-hidden self-stretch"
+              />
+              <p className="text-green-400 text-base font-semibold leading-5 tracking-normal self-center whitespace-nowrap font-['NEXON']">
+                스포츠 </p>
             </Link>
           </div>
-          <div className="shadow-sm bg-white flex flex-col flex-1 px-9 py-3 rounded-3xl max-md:px-5">
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/b5be54af-1423-4122-832e-b3f9b14439a0?apiKey=d9a6bade01504f228813cd0dfee9b81b&"
-              className="aspect-square object-contain object-center w-full overflow-hidden self-stretch"
-            />
-            <Link to={"/trend"} state={{fieldValue : 50000003}} 
-              className="text-violet-500 text-base font-semibold leading-5 tracking-normal self-center whitespace-nowrap font-['NEXON']"
-            >
-              가전
+          <div className="text-center shadow-sm bg-white flex flex-col flex-1 px-9 py-3 rounded-3xl max-md:px-5">
+            <Link to={"/trend"} state={{ fieldValue: 50000003 }}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/b5be54af-1423-4122-832e-b3f9b14439a0?apiKey=d9a6bade01504f228813cd0dfee9b81b&"
+                className="aspect-square object-contain object-center w-full overflow-hidden self-stretch"
+              />
+              <p className="text-violet-500 text-base font-semibold leading-5 tracking-normal self-center whitespace-nowrap font-['NEXON']">
+                가전제품 </p>
             </Link>
           </div>
-          <div className="shadow-sm bg-white flex flex-col flex-1 pl-8 pr-8 py-3 rounded-3xl max-md:px-5">
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/06873a76-9dd5-4c33-ae2f-c5a3541d3134?apiKey=d9a6bade01504f228813cd0dfee9b81b&"
-              className="aspect-square object-contain object-center w-full overflow-hidden self-stretch"
-            />
-            <Link to={"/trend"} state={{fieldValue : 50000007}} 
-              className="text-orange-300 text-base font-semibold leading-5 tracking-normal self-center whitespace-nowrap font-['NEXON']"
-            >
-              여가
+          <div className="text-center shadow-sm bg-white flex flex-col flex-1 pl-8 pr-8 py-3 rounded-3xl max-md:px-5">
+            <Link to={"/trend"} state={{ fieldValue: 50000011 }}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/5212e9aa-1b66-46b3-ad02-878575df8d4f?"
+                className="aspect-square object-contain object-center w-full overflow-hidden self-stretch"
+              />
+              <p className="text-orange-300 text-base font-semibold leading-5 tracking-normal self-center whitespace-nowrap font-['NEXON']">
+                도서 </p>
             </Link>
           </div>
-          <div className="shadow-sm bg-white flex flex-col flex-1 pl-8 pr-9 py-3 rounded-3xl max-md:px-5">
-            <img
-              loading="lazy"
-              src="https://cdn.builder.io/api/v1/image/assets/TEMP/4bca508f-3345-4741-9e79-9cd7499ae077?apiKey=d9a6bade01504f228813cd0dfee9b81b&"
-              className="aspect-square object-contain object-center w-full overflow-hidden self-stretch"
-            />
-            <Link to={"/trend"} state={{fieldValue : 50000006}} 
-              className="text-red-400 text-base font-semibold leading-5 tracking-normal self-center whitespace-nowrap font-['NEXON']"
-            >
-              식품
+          <div className="text-center shadow-sm bg-white flex flex-col flex-1 pl-8 pr-9 py-3 rounded-3xl max-md:px-5">
+            <Link to={"/trend"} state={{ fieldValue: 50000006 }}>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/4bca508f-3345-4741-9e79-9cd7499ae077?apiKey=d9a6bade01504f228813cd0dfee9b81b&"
+                className="aspect-square object-contain object-center w-full overflow-hidden self-stretch"
+              />
+              <p className="text-red-400 text-base font-semibold leading-5 tracking-normal self-center whitespace-nowrap font-['NEXON']">
+                식품 </p>
             </Link>
           </div>
         </div>
